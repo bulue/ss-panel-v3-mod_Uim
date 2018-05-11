441ba5bfcd74d7500b6a01f207df7515a514d4bf
Quine <xiaozhuhaoa@gmail.com>
2018-05-12 00:39:26 +0800

    修改节点列表的显示

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -295,7 +295,7 @@ class UserController extends BaseController
         }
         return FALSE;
     }
-  
+
     public function codepay($request, $response, $args)
     {
         $codepay_id=Config::get('codepay_id');//这里改成码支付ID
@@ -307,7 +307,7 @@ class UserController extends BaseController
         $data = array(
             "id" => $codepay_id,//你的码支付ID
             "pay_id" => $uid, //唯一标识 可以是用户ID,用户名,session_id(),订单ID,ip 付款后返回
-            "type" => $type,//1支付宝支付 2QQ钱包 3微信支付 
+            "type" => $type,//1支付宝支付 2QQ钱包 3微信支付
             "price" => $price,//金额100元
             "param" => "",//自定义参数
             "notify_url"=> $url.'/codepay_callback',//通知地址
@@ -334,7 +334,7 @@ class UserController extends BaseController
         $url = "https://codepay.fateqq.com:51888/creat_order/?".$query; //支付页面
 
 
-        header("Location:".$url);       
+        header("Location:".$url);
     }
 
 
@@ -682,10 +682,10 @@ class UserController extends BaseController
                     }
                 }
 
-                if ($node_loadtemp=$node->getNodeLoad()[0]['load']){   
-                    $node_latestload[$temp[0]]=((float)explode(" ", $node_loadtemp)[0])*100;   
-                } else {   
-                    $node_latestload[$temp[0]]=null;   
+                if ($node_loadtemp=$node->getNodeLoad()[0]['load']){
+                    $node_latestload[$temp[0]]=((float)explode(" ", $node_loadtemp)[0])*100;
+                } else {
+                    $node_latestload[$temp[0]]=null;
                 }
 
 
@@ -696,7 +696,7 @@ class UserController extends BaseController
         $node_prefix=(object)$node_prefix;
         $node_order=(object)$node_order;
         $tools = new Tools();
-        return $this->view()->assign('relay_rules', $relay_rules)->assign('node_class', $node_class)->assign('node_isv6', $node_isv6)->assign('tools', $tools)->assign('node_method', $node_method)->assign('node_muport', $node_muport)->assign('node_bandwidth', $node_bandwidth)->assign('node_heartbeat', $node_heartbeat)->assign('node_prefix', $node_prefix)->assign('node_prealive', $node_prealive)->assign('node_order', $node_order)->assign('user', $user)->assign('node_alive', $node_alive)->assign('node_latestload', $node_latestload)->display('user/node.tpl');
+        return $this->view()->assign('relay_rules', $relay_rules)->assign('node_class', $node_class)->assign('node_isv6', $node_isv6)->assign('tools', $tools)->assign('node_method', $node_method)->assign('node_muport', $node_muport)->assign('node_bandwidth', $node_bandwidth)->assign('node_heartbeat', $node_heartbeat)->assign('node_prefix', $node_prefix)->assign('node_prealive', $node_prealive)->assign('node_order', $node_order)->assign('user', $user)->assign('node_alive', $node_alive)->assign('node_latestload', $node_latestload)->registerClass("URL", "App\Utils\URL")->display('user/node.tpl');
     }
 
 
@@ -1738,7 +1738,7 @@ class UserController extends BaseController
                 $res['msg'] = "管理员不允许删除,如需删除请联系管理员.";
             }
         }
-        
+
     }
     return $this->echoJson($response, $res);
 }
